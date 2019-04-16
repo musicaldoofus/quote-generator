e2db812845ac56f1de6f1478477d1b34e9a2884d
musicaldoofus <burns.trainingdesign@gmail.com>
2019-04-16 15:41:41 -0700

    Updates

---
+++ precache-manifest.aa7f297eae6f32ea46082b50bcdfd960.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b42996cbb3818e11c29d",
+    "url": "/quote-generator/static/js/runtime~main.4226e67e.js"
+  },
+  {
+    "revision": "ecfe02eb2a90a7b629c8",
+    "url": "/quote-generator/static/js/main.618a59a2.chunk.js"
+  },
+  {
+    "revision": "5c7badd242f9ece752a2",
+    "url": "/quote-generator/static/js/2.adfc45b8.chunk.js"
+  },
+  {
+    "revision": "ecfe02eb2a90a7b629c8",
+    "url": "/quote-generator/static/css/main.66d8de21.chunk.css"
+  },
+  {
+    "revision": "aae3309b86199dc59ef57558301ddb20",
+    "url": "/quote-generator/index.html"
+  }
+];
